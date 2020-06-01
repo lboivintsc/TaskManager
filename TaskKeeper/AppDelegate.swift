30543d9051fae6a1dc9d00d07856fe2f5c01ccf6
Lindsey Boivin <l.boivin@techsmith.com>
2020-06-01 10:47:11 -0400

    Initial Commit

---
+++ TaskKeeper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TaskKeeper
+//
+//  Created by Lindsey Boivin on 6/1/20.
+//  Copyright © 2020 Lindsey Boivin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+   func applicationDidFinishLaunching(_ aNotification: Notification) {
+      // Insert code here to initialize your application
+   }
+
+   func applicationWillTerminate(_ aNotification: Notification) {
+      // Insert code here to tear down your application
+   }
+
+
+}
+
---
+++ TaskKeeper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TaskKeeper
+//
+//  Created by Lindsey Boivin on 6/1/20.
+//  Copyright © 2020 Lindsey Boivin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+
+      // Do any additional setup after loading the view.
+   }
+
+   override var representedObject: Any? {
+      didSet {
+      // Update the view, if already loaded.
+      }
+   }
+
+
+}
+
